1cd1e24c07b1efcbef7da7206ac5919a3ad1393b
toppev <thetoppe5@gmail.com>
2018-11-29 21:12:47 +0200

    now also creates the HelpFrame

--- src/main/java/com/gmail/thetoppe5/screencapture/userhelp/HelpButton.java
+++ src/main/java/com/gmail/thetoppe5/screencapture/userhelp/HelpButton.java
@@ -9,6 +9,11 @@ import javax.swing.JFrame;
 
 public class HelpButton extends JButton {
 
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
     // TODO add icon
     public HelpButton(JFrame frame) {
         setText("Help");
@@ -16,7 +21,7 @@ public class HelpButton extends JButton {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                new HelpFrame(frame);
+                new HelpFrame(frame).create();
             }
         });
         setCursor(new Cursor(Cursor.HAND_CURSOR));
