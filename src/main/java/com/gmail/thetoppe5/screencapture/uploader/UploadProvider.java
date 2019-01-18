f54a1e514195c0609d35cbd25fd7d5b2bc5948f1
toppev <thetoppe5@gmail.com>
2019-01-18 21:04:24 +0200

    add default provider

--- src/main/java/com/gmail/thetoppe5/screencapture/uploader/UploadProvider.java
+++ src/main/java/com/gmail/thetoppe5/screencapture/uploader/UploadProvider.java
@@ -2,13 +2,29 @@ package com.gmail.thetoppe5.screencapture.uploader;
 
 public class UploadProvider {
 
+    private static IUploader uploader;
+    private static ImgurUploader defaultProvider = new ImgurUploader();
+
     /**
      * Get the current main upload provider as IUploader
      * 
      * @return the current upload provider
      */
     public static IUploader getProvider() {
-        return new ImgurUploader();
+        return uploader == null ? defaultProvider : uploader;
+    }
+
+    public static void setProvider(IUploader uploader) {
+        UploadProvider.uploader = uploader;
+    }
+
+    /**
+     * Get default provider
+     * 
+     * @return default upload provider, ImgurUploader
+     */
+    public static ImgurUploader getDefaultProvider() {
+        return defaultProvider;
     }
 
 }
